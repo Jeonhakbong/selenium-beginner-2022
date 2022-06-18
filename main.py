65fbcd5cd61490cc0d9f21cbfe6594c3408b05d7
Jeonhakbong <90051532+Jeonhakbong@users.noreply.github.com>
2022-06-18 22:32:13 +0900

    Handled js command and sent arguments

--- main.py
+++ main.py
@@ -9,23 +9,36 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 KEYWORD = "buy domain"  # key word we want to search.
 
+
+# options.
 options = webdriver.ChromeOptions()
 options.add_experimental_option(
     "excludeSwitches", ["enable-logging"]
 )  # handling usb device error.
 options.add_experimental_option("detach", True)  # option to be kept open.
 
+# set browser.
 browser = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 browser.get("https://google.com")
 
+# input keyword.
 search_bar = browser.find_element_by_class_name("gLFyf")
-
 search_bar.send_keys(KEYWORD)  # type hello on search bar.
 search_bar.send_keys(Keys.ENTER)  # press enter.
 
-# selenium wait until class="g" located on browser.
-shitty_element = WebDriverWait(browser, 10).until(
-    EC.presence_of_element_located((By.CLASS_NAME, "g"))
+# selenium wait until class="ULSxyf" located on browser.
+except_element = WebDriverWait(browser, 10).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "ULSxyf"))
+)
+
+# can excecute js command and send arguments.
+# browser.execute_script("alert('hello')")
+browser.execute_script(
+    """ 
+    const except = arguments[0]
+    except.parentElement.removeChild(except)
+    """,
+    except_element,
 )
 
 # find search results.
@@ -33,9 +46,6 @@ search_box = browser.find_element_by_id("search")
 search_results = search_box.find_elements_by_class_name("g")
 
 for index, search_result in enumerate(search_results):
-    title = search_result.find_element_by_tag_name("h3")
-    class_name = search_result.get_attribute("class")
-    if len(title.text) > 0:
-        search_result.screenshot(f"screenshots/{KEYWORD}x{index}.png")
+    search_result.screenshot(f"screenshots/{KEYWORD}x{index}.png")
 
 browser.quit()
